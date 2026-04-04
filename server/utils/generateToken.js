25830293fc870d5c9a1043e64507ce9e720cfaaf
Amogh Bhat Nagri <amoghbhatnagri@gmail.com>
2026-04-04 09:07:53 +0530

    fixed cookie same site for prod

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,7 +9,7 @@ export const generateToken = async (userId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === "production", 
-        sameSite: "strict", 
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         maxAge: 1000 * 60 * 60 * 24 * 7, 
     });
 
